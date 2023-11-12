c8de671a803383a88b31876589835a13a1a095af
Zoclhas <zoclhas@gmail.com>
2023-11-12 14:18:49 +0400

    create user collection

---
+++ payload/src/collections/Users/index.ts
@@ -0,0 +1,57 @@
+import { CollectionConfig } from "payload/types";
+
+import adminsAndUser from "./access/adminsAndUser";
+import { ensureFirstUserIsAdmin } from "./hooks/ensureFirstUserIsAdmin";
+import { checkRole } from "./checkRole";
+
+import { admins } from "../../access/admins";
+import { anyone } from "../../access/access";
+
+const Users: CollectionConfig = {
+  slug: "users",
+  auth: true,
+  admin: {
+    useAsTitle: "email",
+    defaultColumns: ["name", "email"],
+  },
+  access: {
+    read: adminsAndUser,
+    create: anyone,
+    update: adminsAndUser,
+    delete: admins,
+    admin: ({ req: { user } }) => checkRole(["admin"], user),
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "roles",
+      type: "select",
+      hasMany: true,
+      defaultValue: ["user"],
+      options: [
+        {
+          label: "admin",
+          value: "admin",
+        },
+        {
+          label: "user",
+          value: "user",
+        },
+      ],
+      hooks: {
+        beforeChange: [ensureFirstUserIsAdmin],
+      },
+      access: {
+        read: admins,
+        create: admins,
+        update: admins,
+      },
+    },
+  ],
+};
+
+export default Users;
--- payload/src/server.ts
+++ payload/src/server.ts
@@ -1,13 +1,13 @@
-import express from 'express'
-import payload from 'payload'
+import express from "express";
+import payload from "payload";
 
-require('dotenv').config()
-const app = express()
+require("dotenv").config();
+const app = express();
 
 // Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin')
-})
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
 
 const start = async () => {
   // Initialize Payload
@@ -15,13 +15,29 @@ const start = async () => {
     secret: process.env.PAYLOAD_SECRET,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+    email: {
+      fromName: process.env.FROM_NAME,
+      fromAddress: process.env.FROM_ADDRESS,
+      transportOptions: {
+        host: process.env.STMP_HOST,
+        auth: {
+          user: process.env.STMP_USER,
+          pass: process.env.STMP_PASS,
+        },
+        port: 465,
+        secure: true,
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+    },
+  });
 
   // Add your own express routes here
 
-  app.listen(3000)
-}
+  app.listen(process.env.PORT || 3001);
+};
 
-start()
+start();
