17feba5f7f767ba0930052fb66f1368fabe6062f
Zoclhas <zoclhas@gmail.com>
2023-11-13 15:22:24 +0400

    automatic grade level set

--- payload/src/collections/Classses.tsx
+++ payload/src/collections/Classses.tsx
@@ -25,7 +25,7 @@ const ClassSections: CollectionConfig = {
       fields: [
         {
           name: "class",
-          type: "text",
+          type: "number",
           required: true,
           admin: { width: "33%" },
         },
@@ -55,7 +55,43 @@ const ClassSections: CollectionConfig = {
           hasMany: false,
           admin: { width: "33%" },
           hooks: {
-            beforeValidate: [(args) => {}],
+            beforeValidate: [
+              (args) => {
+                const { siblingData } = args;
+                const classValue = Number(siblingData["class"]);
+                const levels = {
+                  senior: {
+                    l: 9,
+                    h: 12,
+                  },
+                  middle: {
+                    l: 6,
+                    h: 8,
+                  },
+                  primary: {
+                    l: 1,
+                    h: 5,
+                  },
+                };
+
+                if (
+                  levels.senior.l <= classValue &&
+                  levels.senior.h >= classValue
+                ) {
+                  return "senior";
+                } else if (
+                  levels.middle.l <= classValue &&
+                  levels.middle.h >= classValue
+                ) {
+                  return "middle";
+                } else if (
+                  levels.primary.l <= classValue &&
+                  levels.primary.h >= classValue
+                ) {
+                  return "primary";
+                }
+              },
+            ],
           },
         },
       ],
