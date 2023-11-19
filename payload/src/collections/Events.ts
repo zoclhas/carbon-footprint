f6cd061459cdbd337f5af526e6aff596ea168f45
Zoclhas <zoclhas@gmail.com>
2023-11-19 10:59:52 +0400

    send mail for events

--- payload/src/collections/Events.ts
+++ payload/src/collections/Events.ts
@@ -1,4 +1,6 @@
 import { CollectionConfig } from "payload/types";
+import { slateToHtml } from "@slate-serializers/html";
+import payload from "payload";
 
 const Events: CollectionConfig = {
   slug: "events",
@@ -11,6 +13,9 @@ const Events: CollectionConfig = {
       name: "title",
       type: "text",
       required: true,
+      admin: {
+        position: "sidebar",
+      },
     },
     {
       name: "duration",
@@ -52,7 +57,46 @@ const Events: CollectionConfig = {
       type: "richText",
       required: true,
     },
+    {
+      name: "send_mail",
+      label: "Send Mail",
+      type: "checkbox",
+      required: true,
+      admin: {
+        position: "sidebar",
+      },
+    },
   ],
+  hooks: {
+    afterChange: [
+      async ({ doc }) => {
+        try {
+          if (doc.send_mail) {
+            const { id, description, title } = doc;
+            const html = slateToHtml(description);
+
+            const users = await payload.find({
+              collection: "users",
+            });
+
+            users.docs.forEach(
+              async (user) =>
+                await payload.sendEmail({
+                  from: `${process.env.FROM_NAME} <${process.env.FROM_ADDRESS}>`,
+                  to: user.email,
+                  subject: `New Event: ${title}`,
+                  html:
+                    html +
+                    `<br /><br/><b>View it here: </b><a href="${process.env.FE_URL}/events/${id}">${title}</b>`,
+                })
+            );
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      },
+    ],
+  },
 };
 
 export default Events;
